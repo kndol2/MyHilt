8445e5d00d46cd370223f6472cbc2aeb6b53f472
seoktaelee <stlee@stunitas.com>
2020-07-18 15:18:56 +0900

    Add Hilt Module (Hilt 모듈에 @InstallIn 주석을 지정하여 각 모듈을 사용하거나 설치할 Android 클래스를 Hilt에 알려야 합니다.)

---
+++ app/src/main/java/com/mh/st/myhilt/repository/datasource/network/ApiModule.kt
@@ -0,0 +1,67 @@
+package com.mh.st.myhilt.repository.datasource.network
+
+import com.mh.st.myhilt.BuildConfig
+import com.mh.st.myhilt.repository.Repository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object ApiModule {
+    const val SERVER_HOST = "https://maps.googleapis.com/maps/api/"
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { message ->
+            if (BuildConfig.DEBUG) {
+                Timber.d(message)
+            }
+        })
+
+        if (BuildConfig.DEBUG) {
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            interceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+
+        return interceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(logger: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .apply {
+                addInterceptor(logger)
+                connectTimeout(15, TimeUnit.SECONDS)
+                readTimeout(20, TimeUnit.SECONDS)
+                writeTimeout(15, TimeUnit.SECONDS)
+            }
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(okHttpClient: OkHttpClient): ApiService {
+        return Retrofit.Builder()
+            .baseUrl(SERVER_HOST)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(ApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRepository(apiService: ApiService): Repository = ApiRepository(apiService)
+}
