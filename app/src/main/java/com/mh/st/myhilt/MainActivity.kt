eb842fbd5b83472861f07eda42df38a323ed2a9a
seoktaelee <stlee@stunitas.com>
2020-07-18 15:37:53 +0900

    Add Hilt Inject

--- app/src/main/java/com/mh/st/myhilt/MainActivity.kt
+++ app/src/main/java/com/mh/st/myhilt/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.mh.st.myhilt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.mh.st.myhilt.repository.Repository
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
+/**
+Application 클래스에 Hilt를 설정하고 애플리케이션 수준 구성요소를 사용할 수 있게 되면
+Hilt는 @AndroidEntryPoint 주석이 있는 다른 Android 클래스에 종속 항목을 제공할 수 있습니다.
+ */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var repository: Repository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            val response = repository.fetchPlaces("restaurant",
+                location = "37.5698411, 126.9783927",
+                name = "한식",
+                opennow = true,
+                rankby = "distance",
+                key = "GOOGLE_PLACE_API_KEY")
+
+            Timber.d("response: $response")
+        }
     }
 }
